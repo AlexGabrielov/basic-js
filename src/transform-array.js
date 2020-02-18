683d77bf8969f273e097bbec565961d5a1d4f484
Alex <alex.gabriel@mail.ru>
2020-02-18 12:44:14 +0300

    feat: solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  let res = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--double-next') {
+      res.push(arr[i + 1])
+      continue
+    }
+    if (arr[i] === '--double-prev') {
+      res.push(arr[i - 1])
+      continue
+    }
+    if (arr[i] === '--discard-next') {
+      i++
+      continue
+    }
+    if (arr[i] === '--discard-prev') {
+      res = res.slice(0, res.length - 1)
+      continue
+    }
+    res.push(arr[i])
+
+  }
+
+  return res.filter(e => e !== undefined)
+}
