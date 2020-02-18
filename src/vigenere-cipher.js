181d8005fc83ce39f415b37e0bd41caeb44002c5
Alex <alex.gabriel@mail.ru>
2020-02-18 22:40:39 +0300

    feat: solve cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,50 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  constructor(type = true) {
+    this.type = type ? 'direct' : 'reverse'
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) throw Error()
+
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    let encryptedMessage = ''
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message[i].match(/^[A-Z]$/)) {
+        encryptedMessage += String.fromCharCode(((message[i].charCodeAt() + key[j % key.length].charCodeAt()) % 26) + 97)
+        j++
+      } else {
+        encryptedMessage += message[i]
+      }
     }
+    return this.type === 'reverse' ? encryptedMessage.split('').reverse().join('').toUpperCase() : encryptedMessage.toUpperCase()
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw Error()
+
+    encryptedMessage = encryptedMessage.toUpperCase()
+    key = key.toUpperCase()
+    let message = ''
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i].match(/^[A-Z]$/)) {
+        let difference = encryptedMessage[i].charCodeAt() - key[j % key.length].charCodeAt()
+        if (difference >= 0) {
+          message += String.fromCharCode((difference % 26) + 97)
+          j++
+        } else {
+          message += String.fromCharCode((26 + difference % 26) + 97)
+          j++
+        }
+      } else {
+        message += encryptedMessage[i]
+      }
+    }
+    return this.type === 'reverse' ? message.split('').reverse().join('').toUpperCase() : message.toUpperCase()
+  }
+
 }
 
 module.exports = VigenereCipheringMachine;
